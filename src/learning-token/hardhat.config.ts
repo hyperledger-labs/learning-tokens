63c10d59cb431db81d7927c3a86b83d9004ed896
neoz666 <neoz.blockchain@gmail.com>
2024-05-07 22:48:58 +0530

    Removed Redundancy

--- src/learning-token/hardhat.config.ts
+++ src/learning-token/hardhat.config.ts
@@ -24,10 +24,6 @@ const {
 }: any = process.env;
 const config: HardhatUserConfig = {
   networks: {
-    sepolia: {
-      url: SEPOLIA_RPC_URL,
-      accounts: [SEPOLIA_PRIV_KEY],
-    },
     // localhost: {
     //   url: "http://localhost:8545", // Replace with your local Hardhat node URL
     //   chainId: 31337, // Replace with the chain ID of your local Hardhat network
