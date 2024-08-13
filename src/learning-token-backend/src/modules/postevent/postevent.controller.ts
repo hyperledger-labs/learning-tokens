47f655e1862e1bd776d6e68578896ecc2a6b2561
khairul111010 <khairul18103199@gmail.com>
2024-08-13 21:15:18 +0600

    postevent
    Signed-off-by: khairul111010 <khairul18103199@gmail.com>

--- src/learning-token-backend/src/modules/postevent/postevent.controller.ts
+++ src/learning-token-backend/src/modules/postevent/postevent.controller.ts
@@ -1,36 +1,63 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
-import { PosteventService } from './postevent.service';
-import { CreatePosteventDto } from './dto/create-postevent.dto';
-import { UpdatePosteventDto } from './dto/update-postevent.dto';
-import { SecretKeyGuard } from 'src/secret-key/secret-key.guard';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+    UseGuards,
+    HttpStatus
+} from '@nestjs/common'
+import { PosteventService } from './postevent.service'
+import { CreatePosteventDto } from './dto/create-postevent.dto'
+import { UpdatePosteventDto } from './dto/update-postevent.dto'
+import { SecretKeyGuard } from 'src/secret-key/secret-key.guard'
 
 @Controller('postevent')
 export class PosteventController {
-  constructor(private readonly posteventService: PosteventService) {}
+    constructor(private readonly posteventService: PosteventService) {}
 
-  @UseGuards(SecretKeyGuard)
-  @Post()
-  create(@Body() createPosteventDto: CreatePosteventDto) {
-    return this.posteventService.create(createPosteventDto);
-  }
+    @UseGuards(SecretKeyGuard)
+    @Post()
+    async create(@Body() createPosteventDto: CreatePosteventDto[]) {
+        try {
+            const result = await this.posteventService.create(
+                createPosteventDto
+            )
+            return {
+                status: HttpStatus.CREATED,
+                message: 'Post-event created successfully',
+                result: result
+            }
+        } catch (error) {
+            return {
+                status: HttpStatus.NOT_FOUND,
+                message: 'Something went wrong'
+            }
+        }
+    }
 
-  @Get()
-  findAll() {
-    return this.posteventService.findAll();
-  }
+    @Get()
+    findAll() {
+        return this.posteventService.findAll()
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.posteventService.findOne(+id);
-  }
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.posteventService.findOne(+id)
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePosteventDto: UpdatePosteventDto) {
-    return this.posteventService.update(+id, updatePosteventDto);
-  }
+    @Patch(':id')
+    update(
+        @Param('id') id: string,
+        @Body() updatePosteventDto: UpdatePosteventDto
+    ) {
+        return this.posteventService.update(+id, updatePosteventDto)
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.posteventService.remove(+id);
-  }
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.posteventService.remove(+id)
+    }
 }
