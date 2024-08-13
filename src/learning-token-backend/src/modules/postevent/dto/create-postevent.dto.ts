3a8901064190294b1e18068da5616a73fb842b4e
Khairul Hasan <khairul18103199@gmail.com>
2024-08-13 12:42:14 +0600

    post event

--- src/learning-token-backend/src/modules/postevent/dto/create-postevent.dto.ts
+++ src/learning-token-backend/src/modules/postevent/dto/create-postevent.dto.ts
@@ -1,1 +1,18 @@
-export class CreatePosteventDto {}
+import { IsString } from "class-validator"
+
+export class CreatePosteventDto {
+    @IsString()
+    eventId: string
+
+    @IsString()
+    name: string
+    
+    @IsString()
+    email: string
+    
+    @IsString()
+    joinTime: Date
+    
+    @IsString()
+    leaveTime: Date
+}
