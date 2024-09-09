3768aa9a79806cfb5999f986fc1aa3279ba3f996
weber77 <weberkanga77@gmail.com>
2024-09-09 04:33:47 +0100

    update: tables and swtiches
    Signed-off-by: weber77 <weberkanga77@gmail.com>

--- src/learning-token-dashboard/src/pages/instructor/index.tsx
+++ src/learning-token-dashboard/src/pages/instructor/index.tsx
@@ -1,4 +1,4 @@
-import { Loader, Table, Toggle } from "rsuite";
+import { Loader, Toggle } from "rsuite";
 import {
   useLazyGetInstructorQuery,
   useSmartContractCallMutation,
@@ -9,7 +9,15 @@ import { useEffect } from "react";
 import Pagination from "../../components/Pagination";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
-const { Column, HeaderCell, Cell } = Table;
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+} from "@/components/ui/table";
+
 const Instructor = () => {
   const auth = useSelector((state: RootState) => state.auth);
   const [getInstructor, { data, isLoading }] = useLazyGetInstructorQuery();
@@ -38,6 +46,7 @@ const Instructor = () => {
       updateInstructorStatus(rowData);
     });
   };
+
   if (isLoading) {
     return (
       <div className="flex items-center justify-center">
@@ -45,52 +54,50 @@ const Instructor = () => {
       </div>
     );
   }
+
   return (
     <div className="py-3">
-      <Table
-        data={data?.result?.data}
-        autoHeight
-        rowClassName={"cursor-pointer"}
-      >
-        <Column flexGrow={1} align="center" fixed>
-          <HeaderCell>Id</HeaderCell>
-          <Cell dataKey="id" />
-        </Column>
+      {data?.result?.data.length > 0 ? (
+        <>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="text-center">Id</TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Public Address</TableHead>
+                <TableHead>Status</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {data?.result?.data.map((rowData: any) => (
+                <TableRow key={rowData.id} className="cursor-pointer">
+                  <TableCell className="text-center">{rowData.id}</TableCell>
+                  <TableCell className="capitalize">{rowData.name}</TableCell>
+                  <TableCell>{rowData.publicAddress}</TableCell>
+                  <TableCell>
+                    <Toggle
+                      checked={rowData.status}
+                      onClick={() => toggleStatus(rowData)}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
 
-        <Column flexGrow={1}>
-          <HeaderCell>Name</HeaderCell>
-          <Cell dataKey="name" className="capitalize" />
-        </Column>
-
-        <Column flexGrow={1}>
-          <HeaderCell>Public Address</HeaderCell>
-          <Cell dataKey="publicAddress" />
-        </Column>
-
-        <Column flexGrow={1}>
-          <HeaderCell>Status</HeaderCell>
-          <Cell>
-            {(rowData: any) => {
-              return (
-                <>
-                  <Toggle
-                    checked={rowData.status}
-                    onClick={() => toggleStatus(rowData)}
-                  />
-                </>
-              );
-            }}
-          </Cell>
-        </Column>
-      </Table>
-
-      <Pagination
-        total={data?.result?.pagination.totalCount || 0}
-        activePage={pagination.page || 1}
-        limit={pagination.limit || 10}
-        onChangePage={pagination.handleChangePage}
-        onChangeLimit={pagination.handleChangeLimit}
-      />
+          <Pagination
+            total={data?.result?.pagination.totalCount || 0}
+            activePage={pagination.page || 1}
+            limit={pagination.limit || 10}
+            onChangePage={pagination.handleChangePage}
+            onChangeLimit={pagination.handleChangeLimit}
+          />
+        </>
+      ) : (
+        <div className="text-center py-4 text-gray-500">
+          No instructors found.
+        </div>
+      )}
     </div>
   );
 };
