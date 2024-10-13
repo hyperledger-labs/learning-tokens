81af6d3fd1282995a707cdd311f65d6aa3ba70bd
TanjinAlam <piash.tanjin@gmail.com>
2024-10-14 01:40:56 +0600

    pre-event user list api and instructor added

--- src/learning-token-backend/src/modules/smartcontract/enums/smartcontract-functions.enum.ts
+++ src/learning-token-backend/src/modules/smartcontract/enums/smartcontract-functions.enum.ts
@@ -2,4 +2,5 @@ export enum SmartcontractFunctionsEnum {
     REGISTER_INSTITUTION = 'registerInstitution',
     REGISTER_INSTRUCTOR = 'registerInstructor',
     REGISTER_LEARNER = 'registerLearner',
+    ADD_INSTRUCTOR_TO_INSTITUTION = 'addInstructorToInstitution'
 }
