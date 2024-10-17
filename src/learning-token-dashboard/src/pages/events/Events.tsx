e36814ebbbaa07e026c679b9769f4912237247bd
skyrocket55 <67418626+skyrocket55@users.noreply.github.com>
2024-10-16 20:26:42 -0400

    preevents list page using bootstrap and datetime formatter
    Signed-off-by: skyrocket55 <67418626+skyrocket55@users.noreply.github.com>

--- src/learning-token-dashboard/src/pages/events/Events.tsx
+++ src/learning-token-dashboard/src/pages/events/Events.tsx
@@ -1,70 +1,101 @@
 import { useNavigate } from "react-router-dom";
-import { Table } from "rsuite";
-const { Column, HeaderCell, Cell } = Table;
 import axios from "axios";
 import { useEffect, useState } from "react";
+import usePagination from "../../hooks/usePagination";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+import { formatDateTime } from "../../utils";
+import { Table, Container, Pagination as BootstrapPagination } from "react-bootstrap";
 
 const Events = () => {
   const navigate = useNavigate();
   const [tableData, setTableData] = useState([]);
+  const [totalItems, setTotalItems] = useState(0);
   const handleRowclick = (data: any) => {
     navigate(`/events/${data.eventId}`);
   };
+  const pagination = usePagination();
+  const auth = useSelector((state: RootState) => state.auth);
 
   useEffect(() => {
     const fetchPreeventData = async () => {
       try {
-        const response = await axios.get(`${import.meta.env.VITE_API_URL}/preevent`);
-        console.log(`response::: ${response}`);
-        
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/preevent`, {
+          params: {
+            page: pagination.page,
+            limit: pagination.limit,
+          },
+          headers: {
+            Authorization: `Bearer ${auth.accessToken}`,
+          },
+        });
+
         setTableData(response?.data?.result || []);
+        setTotalItems(response?.data?.meta?.totalItems || 0);
       } catch (error) {
         console.error("Error fetching preevent data:", error);
       }
     };
 
     fetchPreeventData();
-  }, []);
+  }, [pagination.page, pagination.limit]);
+
+  // Custom cell component to format the event date
+  const DateCell = ({ eventDate }: { eventDate: string }) => {
+    return <span>{formatDateTime(eventDate)}</span>;
+  };
+
+  const SpeakersNameCell = ({ speakersName }: { speakersName: string[] }) => {
+    return <span>{speakersName.join(", ")}</span>;
+  }
+
+  const totalPages = Math.ceil(totalItems / pagination.limit); //For Bootstrap Pagination
 
   return (
-    <Table
-      data={tableData}
-      autoHeight
-      rowClassName={"cursor-pointer"}
-      onRowClick={handleRowclick}
-    >
-      <Column flexGrow={1} align="center" fixed>
-        <HeaderCell>Event Id</HeaderCell>
-        <Cell dataKey="eventId" />
-      </Column>
+    <Container>
+      <div className="table-responsive">
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th className="text-center" style={{ width: "5%" }}>Event Id</th>
+              <th className="text-center" style={{ width: "10%" }}>Event Name</th>
+              <th className="text-center" style={{ width: "25%" }}>Description</th>
+              <th className="text-center" style={{ width: "12%" }}>Event Date</th>
+              <th className="text-center" style={{ width: "10%" }}>Organizer</th>
+              <th className="text-center" style={{ width: "20%" }}>Speaker's Name</th>
+              <th className="text-center" style={{ width: "10%" }}>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableData.map((event) => (
+              <tr key={event.meetingEventId} onClick={() => navigate(`/events/${event.meetingEventId}`)} className="cursor-pointer">
+                <td className="text-center">{event.meetingEventId}</td>
+                <td className="text-center">{event.eventName}</td>
+                <td className="text-center">{event.description}</td>
+                <td className="text-center"><DateCell eventDate={event.eventDate} /></td>
+                <td className="text-center">{event.organizerName}</td>
+                <td className="text-center"><SpeakersNameCell speakersName={event.speakersName} /></td>
+                <td className="text-center">{event.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
 
-      <Column flexGrow={1}>
-        <HeaderCell>Event Name</HeaderCell>
-        <Cell dataKey="eventName" />
-      </Column>
-
-      <Column flexGrow={1}>
-        <HeaderCell>Description</HeaderCell>
-        <Cell dataKey="description" />
-      </Column>
-
-      <Column flexGrow={1}>
-        <HeaderCell>Event Date</HeaderCell>
-        <Cell dataKey="eventDate" />
-      </Column>
-      <Column flexGrow={1}>
-        <HeaderCell>Organizer</HeaderCell>
-        <Cell dataKey="organizer" />
-      </Column>
-      <Column flexGrow={1}>
-        <HeaderCell>Speaker's Name</HeaderCell>
-        <Cell dataKey="speakerName" />
-      </Column>
-      <Column flexGrow={1}>
-        <HeaderCell>Status</HeaderCell>
-        <Cell dataKey="status" />
-      </Column>
-    </Table>
+      <div className="d-flex justify-content-center mt-3">
+        <BootstrapPagination>
+          <BootstrapPagination.Prev
+            onClick={pagination.handlePrev}
+            disabled={pagination.page <= 1 || totalItems === 0}
+          />
+          <BootstrapPagination.Item active>{pagination.page}</BootstrapPagination.Item>
+          <BootstrapPagination.Next
+            onClick={pagination.handleNext}
+            disabled={pagination.page >= totalPages || totalItems === 0}
+          />
+        </BootstrapPagination>
+      </div>
+    </Container>
   );
 };
 
