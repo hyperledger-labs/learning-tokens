23138fc78f39fef0c385307ea6ee978d82698ae6
Neo <115883668+NeoZ666@users.noreply.github.com>
2025-07-22 13:53:26 +0530

    Revert "Initial changes for integration of open edx LMS, written some apis..."

--- npm_package ltsdk/src/index.ts
+++ npm_package ltsdk/src/index.ts
@@ -1,25 +1,3 @@
 // index.ts
 
 export { run } from './zoomprocessor';
-
-// Export edX LMS functions
-export {
-  getEdxUserProfile,
-  getEdxUserEnrollments,
-  getEdxCourseAssessments,
-  getEdxAssessmentQuestions,
-  getEdxAssessmentResponses,
-  getEdxCourseGrade,
-  getEdxCourseParticipants,
-  getEdxCourseInstructors,
-  getEdxAssessmentAttendance,
-  getEdxAssessmentAnalytics,
-  getEdxUserCertificate,
-  getEdxCourses,
-  getEdxAccessToken,
-  // Data processing functions
-  processEdxCourseData,
-  calculateEdxAssessmentScores,
-  aggregateEdxParticipantData,
-  processEdxData
-} from './edxDataprocessor';
